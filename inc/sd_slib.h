f41015f36dace800e690618a150bc4c931d6326a
capn-wiggles <nicolae.nicolaev@hotmail.com>
2018-10-15 18:53:18 +0300

    added config_ddr & config_ddr_out functions

--- inc/sd_slib.h
+++ inc/sd_slib.h
@@ -9,5 +9,14 @@
 #ifndef SD_SLIB_H_
 #define SD_SLIB_H_
 
+#define F_CPU 16000000UL // 16 MHz
+
+typedef unsigned char _BYTE;
+
+_BYTE config_ddr(_BYTE port, _BYTE port_conf);
+_BYTE config_ddr_out(_BYTE port, _BYTE port_nr[8]);
+
+/* MATH FUNCTIONS */
+int pow(int x, int y);
 
 #endif /* SD_SLIB_H_ */
--- sd_slib.c
+++ sd_slib.c
@@ -8,10 +8,39 @@
 #include <avr/io.h>
 #include "inc/sd_slib.h"
 
-
-/* Replace with your library code */
-int myfunc(void)
-{
+_BYTE config_ddr(_BYTE port, _BYTE port_conf) {
+	if(port == 'b' || port == 'B') DDRB=port_conf;
+	else if(port == 'c' || port == 'C') DDRC=port_conf;
+	else if(port == 'd' || port == 'D') DDRD=port_conf;
+	else return 1;
+	
 	return 0;
 }
 
+_BYTE config_ddr_out(_BYTE port, _BYTE port_nr[8]) {
+	unsigned short i = 0;
+	if(port=='b' || port=='B') {
+		DDRB=0b00000000;
+		for(;i<8;i++) DDRB |= pow(2, port_nr[i]);
+	} else if(port == 'c' || port == 'C') {
+		DDRC=0b00000000;
+		for(;i<8;i++) DDRC |= pow(2, port_nr[i]);
+	} else if(port == 'd' || port == 'D') {
+		DDRD=0b00000000;
+		for(;i<8;i++) DDRD |= pow(2, port_nr[i]);
+	} else return 1;
+	
+	return 0;
+}
+
+/* MATH FUNCTIONS */
+int pow(int x, int y) {
+	unsigned short i=0;
+	if(y>0) {
+		for(;i<y;y++) x*=x;
+	} else if(y<0) {
+		for(;i<y;y++) x*=x;
+		x=1/x;
+	} else x=1;
+	return x;
+}
