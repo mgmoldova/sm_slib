7e5db18049736c11805be90654c8751f61831439
capn-wiggles <nicolae.nicolaev@hotmail.com>
2018-10-15 17:46:01 +0300

    first commit

---
+++ inc/sd_slib.h
@@ -0,0 +1,13 @@
+/*
+ * sd_slib.h
+ *
+ * Created: 10/15/2018 5:28:55 PM
+ *  Author: capn-wiggles
+ */ 
+
+
+#ifndef SD_SLIB_H_
+#define SD_SLIB_H_
+
+
+#endif /* SD_SLIB_H_ */
---
+++ sd_slib.c
@@ -0,0 +1,17 @@
+/*
+ * SD_SLIB.c
+ *
+ * Created: 10/15/2018 5:27:52 PM
+ * Author : capn-wiggles
+ */ 
+
+#include <avr/io.h>
+#include "inc/sd_slib.h"
+
+
+/* Replace with your library code */
+int myfunc(void)
+{
+	return 0;
+}
+
